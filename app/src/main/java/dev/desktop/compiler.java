3390e9d5e823ec4349906a347770c40845df2f64
Bruce Li <isanoob1897@outlook.com>
2025-06-24 17:30:23 -0700

    add

---
+++ app/src/main/java/dev/desktop/compiler.java
@@ -0,0 +1,13 @@
+package dev.desktop;
+import java.util.*;
+
+public class compiler {
+    String input;
+    public compiler(String input) {
+        this.input = input;
+        lex(input);
+    }
+    private void lex(String input) {
+        // todo
+    }
+}
